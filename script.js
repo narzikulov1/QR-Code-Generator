3d71bcb315ae436973d2fc8debb42fbf288d3daf
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-18 12:32:44 +0500

    download qr code completed

--- script.js
+++ script.js
@@ -3,16 +3,32 @@ let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
-function generateQR(){
-    if(qrText.value.length > 0){
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+function generateQR() {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value + ".jpg";
         qrText.value = "";
         imgBox.classList.add("show-img");
-    }else{
+    } else {
         qrText.classList.add("error");
-        setTimeout(()=>{
+        setTimeout(() => {
             qrText.classList.remove("error");
-        },1000)
+        }, 1000)
     }
 }
+
+qrImage.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    var api_link = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value + ".jpg";
+    fetch(api_link)
+        .then(response => response.blob())
+        .then(blob => {
+            var link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'api_link.jpg';
+            link.click();
+        });
+})
+
+
 
